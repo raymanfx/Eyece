4a6002026e5af0b1f5f96cb47fccb594a3886c02
Christopher N. Hesse <raymanfx@gmail.com>
2020-07-31 12:28:07 +0200

    Initial controls support
    Signed-off-by: Christopher N. Hesse <raymanfx@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use eye::prelude::*;
 use ffimage::packed::dynamic::ImageView as DynamicImageView;
 
 use iced::{
-    executor, pick_list, scrollable, Application, Column, Command, Element, Length, PickList, Row,
-    Scrollable, Settings, Text,
+    button, executor, pick_list, scrollable, slider, Application, Button, Checkbox, Column,
+    Command, Element, Length, PickList, Row, Scrollable, Settings, Slider, Text,
 };
 
 macro_rules! unwrap_or_return {
@@ -42,6 +42,7 @@ struct Eyece<'a> {
     device: Option<Box<dyn Device>>,
 
     config: Config,
+    controls: Controls,
     log: Log,
 }
 
@@ -49,7 +50,9 @@ struct Eyece<'a> {
 enum Message {
     DeviceSelected(model::device::Node),
     FormatSelected(model::device::Format),
+    ControlChanged(model::device::Control),
     ConfigMessage(ConfigMessage),
+    ControlsMessage(ControlsMessage),
     LogMessage(LogMessage),
 }
 
@@ -146,6 +149,60 @@ impl<'a> Application for Eyece<'a> {
                         }
                     }
 
+                    // enumerate controls
+                    let controls = unwrap_or_return!(
+                        device.query_controls(),
+                        Command::none(),
+                        (|err| self.log.update(LogMessage::Log(
+                            model::log::Level::Warn,
+                            format!(
+                                "Message::DeviceSelected: Failed to query controls ({})",
+                                err
+                            ),
+                        )))
+                    );
+                    self.controls.controls = controls
+                        .iter()
+                        .map(|ctrl| {
+                            let model = model::device::Control::from(ctrl);
+                            let state = match &model.representation {
+                                model::device::ControlRepresentation::Button => {
+                                    Some(ControlState::Button(button::State::default()))
+                                }
+                                model::device::ControlRepresentation::Boolean => None,
+                                model::device::ControlRepresentation::Integer(_) => {
+                                    Some(ControlState::Slider(slider::State::default()))
+                                }
+                                _ => None,
+                            };
+                            (model, state)
+                        })
+                        .collect();
+
+                    // query control values
+                    for (control, _) in &mut self.controls.controls {
+                        let value;
+                        match &control.representation {
+                            model::device::ControlRepresentation::Boolean
+                            | model::device::ControlRepresentation::Integer(_) => {
+                                value = unwrap_or_return!(
+                                    device.control(control.id),
+                                    Command::none(),
+                                    (|err| self.log.update(LogMessage::Log(
+                                        model::log::Level::Warn,
+                                        format!(
+                                            "Message::DeviceSelected: Failed to read control ({})",
+                                            err
+                                        ),
+                                    )))
+                                );
+
+                                control.value = value;
+                            }
+                            _ => continue,
+                        }
+                    }
+
                     self.device = Some(device);
                     unsafe {
                         self.stream = Some(mem::transmute(
@@ -209,8 +266,49 @@ impl<'a> Application for Eyece<'a> {
                     ),
                 ));
             }
+            Message::ControlChanged(control) => {
+                let device = self.device.as_mut().unwrap();
+
+                for ctrl in &mut self.controls.controls {
+                    if ctrl.0.id == control.id {
+                        unwrap_or_return!(
+                            device.set_control(ctrl.0.id, &control.value),
+                            Command::none(),
+                            (|err| self.log.update(LogMessage::Log(
+                                model::log::Level::Warn,
+                                format!(
+                                    "Message::ControlChanged: Failed to write control ({})",
+                                    err
+                                ),
+                            )))
+                        );
+
+                        // we successfully wrote the control value, now update its state
+                        ctrl.0.value = control.value.clone();
+                    }
+                }
+
+                let value_str = match &control.value {
+                    model::device::ControlValue::None => "None".to_string(),
+                    model::device::ControlValue::String(val) => val.to_string(),
+                    model::device::ControlValue::Boolean(val) => val.to_string(),
+                    model::device::ControlValue::Integer(val) => val.to_string(),
+                };
+                self.log.update(LogMessage::Log(
+                    model::log::Level::Info,
+                    format!(
+                        "Message::ControlChanged: ID: {}, Value: {}",
+                        control.id, value_str
+                    ),
+                ));
+            }
             Message::ConfigMessage(msg) => {
                 for msg in self.config.update(msg) {
+                    self.update(msg);
+                }
+            }
+            Message::ControlsMessage(msg) => {
+                for msg in self.controls.update(msg) {
                     self.update(msg);
                 }
             }
@@ -229,6 +327,11 @@ impl<'a> Application for Eyece<'a> {
         Column::new()
             .padding(PADDING)
             .push(self.config.view().map(|msg| Message::ConfigMessage(msg)))
+            .push(
+                self.controls
+                    .view()
+                    .map(|msg| Message::ControlsMessage(msg)),
+            )
             .push(self.log.view().map(|msg| Message::LogMessage(msg)))
             .into()
     }
@@ -294,6 +397,116 @@ impl Config {
                 ConfigMessage::FormatSelected,
             ))
             .into()
+    }
+}
+
+#[derive(Debug, Default, Clone)]
+struct Controls {
+    state: scrollable::State,
+    controls: Vec<(model::device::Control, Option<ControlState>)>,
+}
+
+#[derive(Debug, Clone)]
+enum ControlState {
+    Button(button::State),
+    Slider(slider::State),
+}
+
+#[derive(Debug, Clone)]
+enum ControlsMessage {
+    ControlChanged(model::device::Control),
+}
+
+impl Controls {
+    fn update(&mut self, message: ControlsMessage) -> Vec<Message> {
+        match message {
+            ControlsMessage::ControlChanged(ctrl) => vec![
+                Message::LogMessage(LogMessage::Log(
+                    model::log::Level::Info,
+                    format!("ControlsMessage::ControlChanged: ID: {}", ctrl.id),
+                )),
+                Message::ControlChanged(ctrl),
+            ],
+        }
+    }
+
+    fn view(&mut self) -> Element<ControlsMessage> {
+        // Uniform padding and spacing for all elements.
+        const SPACING: u16 = 10;
+        const PADDING: u16 = 10;
+
+        // Device controls
+        let mut controls = Scrollable::new(&mut self.state).width(Length::Fill);
+        for (control, state) in &mut self.controls {
+            let control_clone = control.clone();
+
+            match &control.representation {
+                model::device::ControlRepresentation::Button => {
+                    let state = match state.as_mut().unwrap() {
+                        ControlState::Button(state) => state,
+                        _ => panic!("Wrong button state"),
+                    };
+                    controls = controls.push(
+                        Row::new()
+                            .spacing(SPACING)
+                            .push(Text::new(control.name.clone()))
+                            .push(
+                                Button::new(state, Text::new("Toggle"))
+                                    .on_press(ControlsMessage::ControlChanged(control_clone)),
+                            ),
+                    );
+                }
+                model::device::ControlRepresentation::Boolean => {
+                    let checked = match control.value {
+                        model::device::ControlValue::Boolean(val) => Some(val),
+                        model::device::ControlValue::Integer(val) => Some(val != 0),
+                        _ => None,
+                    };
+                    controls = controls.push(
+                        Row::new()
+                            .spacing(SPACING)
+                            .push(Text::new(control.name.clone()))
+                            .push(Checkbox::new(checked.unwrap(), "", move |val| {
+                                let mut control = control_clone.clone();
+                                control.value = model::device::ControlValue::Boolean(val);
+                                ControlsMessage::ControlChanged(control)
+                            })),
+                    );
+                }
+                model::device::ControlRepresentation::Integer(repr) => {
+                    let state = match state.as_mut().unwrap() {
+                        ControlState::Slider(state) => state,
+                        _ => panic!("Wrong slider state"),
+                    };
+                    let value = match control.value {
+                        model::device::ControlValue::Integer(val) => Some(val),
+                        _ => None,
+                    };
+                    controls = controls.push(
+                        Row::new()
+                            .spacing(SPACING)
+                            .push(Text::new(control.name.clone()))
+                            .push(
+                                Slider::new(
+                                    state,
+                                    (repr.range.0 as f64)..=(repr.range.1 as f64),
+                                    value.unwrap() as f64,
+                                    move |val| {
+                                        let mut control = control_clone.clone();
+                                        control.value =
+                                            model::device::ControlValue::Integer(val as i64);
+                                        ControlsMessage::ControlChanged(control)
+                                    },
+                                )
+                                .step(repr.step as f64),
+                            ),
+                    );
+                }
+                _ => continue,
+            }
+        }
+
+        Column::new().padding(PADDING).push(controls).into()
     }
 }
 
--- src/model/device.rs
+++ src/model/device.rs
@@ -1,4 +1,4 @@
-use eye::device::Info as DeviceInfo;
+use eye::device::{ControlInfo, Info as DeviceInfo};
 
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
 pub struct Node {
@@ -32,3 +32,41 @@ impl std::fmt::Display for Format {
         write!(f, "{}x{}", self.width, self.height)
     }
 }
+
+#[derive(Clone)]
+pub struct Control {
+    pub id: u32,
+    pub name: String,
+
+    pub representation: ControlRepresentation,
+    pub value: ControlValue,
+}
+
+impl core::convert::From<&ControlInfo> for Control {
+    fn from(info: &ControlInfo) -> Self {
+        Control {
+            id: info.id,
+            name: info.name.clone(),
+            representation: info.repr.clone(),
+            value: ControlValue::None,
+        }
+    }
+}
+
+impl std::fmt::Debug for Control {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Control")
+            .field("id", &self.id)
+            .field("name", &self.name)
+            .finish()
+    }
+}
+
+impl std::fmt::Display for Control {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.name)
+    }
+}
+
+pub type ControlRepresentation = eye::control::Representation;
+pub type ControlValue = eye::control::Value;
